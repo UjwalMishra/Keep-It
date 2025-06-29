9904b9bc928f09bdf0371f6d9998457f3ca728ad
Ujwal <ujwalcse01@gmail.com>
2025-06-29 13:09:40 +0530

    Backend : signin controller

--- backend/src/controllers/Auth.ts
+++ backend/src/controllers/Auth.ts
@@ -1,10 +1,18 @@
 import { User } from "../models/User";
-import { auth } from "../zod-schema/Auth";
+import { authZodSchema } from "../zod-schema/Auth";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-export const signupController = async (req: any, res: any) => {
+dotenv.config();
+
+export const signupController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
   try {
     const userData = req.body;
-    const { success } = auth.safeParse(userData);
+    const { success } = authZodSchema.safeParse(userData);
 
     if (!success) {
       return res.status(411).json({
@@ -41,4 +49,56 @@ export const signupController = async (req: any, res: any) => {
   }
 };
 
-export const signinController = async () => {};
+export const signinController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const userData = req.body;
+    const { success } = authZodSchema.safeParse(userData);
+
+    if (!success) {
+      return res.status(411).json({
+        msg: "You have sent the wrong input",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ username: userData.username });
+    if (!user) {
+      return res.status(403).json({
+        msg: "User does not exists",
+        success: false,
+      });
+    }
+
+    if (userData.password !== user.password) {
+      return res.status(403).json({
+        msg: "Wrong Password",
+        success: false,
+      });
+    }
+    const payload = {
+      userId: user._id,
+      username: user.username,
+    };
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error("JWT_SECRET is not defined in environment variables");
+    }
+
+    const token = jwt.sign(payload, secret);
+
+    return res.status(200).json({
+      token: token,
+      success: true,
+    });
+  } catch (err) {
+    console.log("Error during user signin ", err);
+
+    return res.status(500).json({
+      msg: "Server failure",
+    });
+  }
+};
--- backend/src/zod-schema/Auth.ts
+++ backend/src/zod-schema/Auth.ts
@@ -1,7 +1,7 @@
 import zod, { string } from "zod";
 
 //signup
-export const auth = zod.object({
+export const authZodSchema = zod.object({
   username: zod.string().min(1),
   password: zod.string().min(5),
 });
