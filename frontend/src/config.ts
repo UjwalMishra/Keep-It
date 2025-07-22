1ff6fdeee31d18c9c7c630cba0df5577a4319cda
Ujwal <ujwalcse01@gmail.com>
2025-07-22 17:12:34 +0530

    Frontend : update

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:3000/api/v1";
+export const BACKEND_URL = "https://keep-it-ujwal-mishras-projects.vercel.app/";
 
 export const FRONTEND_URL = "http://localhost:5173/";
